afc4ab1aa2841e2cde84237af8179a04921c6b10
league <maddoxseul@sbsd.net>
2023-10-27 18:23:37 -0700

    bobby is chuggie II

--- _02_random/_4_validation/validation.py
+++ _02_random/_4_validation/validation.py
@@ -10,7 +10,14 @@ if __name__ == '__main__':
     print(random_number)
 
     # TODO 1) Use each value of random_number to give the user a random compliment
-
+    for i in range(10):
+        shrek = simpledialog.askstring(title='shrek', prompt='enter a number, 1 to 5')
+    else:
+        messagebox.showerror(title='shhhshshsshshshshshshsh', message='U SUCK MAN!!!')
+        if shrek == 1:
+            messagebox.showinfo(title='gsjsgjsjgsdd', message=' u r good')
+        elif shrek == 2:
+            messagebox.showinfo()
     # TODO 2) Repeat all the code above 10 times
 
     # TODO 3) Find someone to test out your program. They will like it :)
